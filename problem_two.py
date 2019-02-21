f3aa248b66027e29fc99a6f4c9517761e889fb3b
HouariZEGAI <houarizegai14@gmail.com>
2019-02-21 22:23:05 +0100

    Problem #2 solved

--- problem_two.py
+++ problem_two.py
@@ -1,1 +1,14 @@
 """Write a program which can compute the factorial of a given numbers."""
+
+number = int(input("Type number: "))
+
+if(number < 0):
+	print("Invalid input (negative number!)")
+else:
+	result = 1
+	index = 2
+	while index <= number:
+		result *= index
+		index += 1
+	
+	print(number, '! = ', result)
