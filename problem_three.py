f9dd8ea02a764995fcf7b5e3784e97bbe7ed3c2e
HouariZEGAI <houarizegai14@gmail.com>
2019-02-21 22:36:59 +0100

    Problem #3 solved

--- problem_three.py
+++ problem_three.py
@@ -1,1 +1,15 @@
 """Write a program that solves second degree equation of the form ax^2 +bx + c = 0."""
+
+a = int(input('Type a: '))
+b = int(input('Type b: '))
+c = int(input('Type c: '))
+
+delta = b ** 2 - 4 * a * c
+
+if delta < 0:
+	print("The is no solution because delta < 0 !")
+elif delta == 0:
+	print('solution is : ', -b / 2*a)
+else:
+	print('solution 1 is: ', (-b - delta ** (1.0 / 2)) / 2 * a)
+	print('solution 2 is: ', (-b + delta ** (1.0 / 2)) / 2 * a)
